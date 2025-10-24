b5625a93da2ebf919d51563646c991a12add8d38
Benjamin Marfull <benja.marfull@gmail.com>
2025-10-24 12:38:41 -0300

    se configura test unitarios

---
+++ karma.conf.js
@@ -0,0 +1,73 @@
+const path = require('path');
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'src/setupTests.js',
+      'src/**/*.spec.js',
+      'src/**/*.test.js'
+    ],
+    preprocessors: {
+      'src/setupTests.js': ['webpack'],
+      'src/**/*.spec.js': ['webpack'],
+      'src/**/*.test.js': ['webpack']
+    },
+    webpack: {
+      mode: 'development',
+      module: {
+        rules: [
+          // JSX y JS
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                  ['@babel/preset-react', { runtime: 'automatic', development: true }]
+                ]
+              }
+            }
+          },
+          // CSS
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'] // 👈 Agregado
+          },
+          // Imágenes
+          {
+            test: /\.(png|jpe?g|gif|webp|svg)$/i,
+            type: 'asset/inline'
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+          fireapp: path.resolve(__dirname, 'src/firebase.js'), // Ajusta según tu ruta
+          fireauth: path.resolve(__dirname, 'src/components/firebaseUtils.js') // Ajusta según tu ruta
+        }
+      }
+    },
+
+    reporters: ['spec', 'coverage'],
+    specReporter: {
+      suppressPassed: false,
+      suppressSkipped: true,
+      showSpecTiming: true
+    },
+
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
+
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    colors: true,
+    logLevel: config.LOG_INFO
+  });
+};
---
+++ src/pages/Home.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
+
+describe('Elementos del Home', () => {
+  it('Busca titulo de productos', async () => {
+    render(<Home />);
+    const titulo = await screen.findByText(/Nuestros Productosod/i);
+    expect(titulo).toBeInTheDocument();
+  }); 
+});

35dd09e49a4c327756712554c49ca2ca1464344f
Benjamin Marfull <benja.marfull@gmail.com>
2025-11-04 23:46:57 -0300

    se modifica estructura para alojar backend y se da estructura inicial para Springboot

---
+++ backEnd/src/main/java/cl/duoc/Level_Up/LevelUpApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.Level_Up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpApplication.class, args);
+	}
+
+}

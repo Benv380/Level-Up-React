c9df250419eb2efe42dfac91d389b67737bffd64
Benjamin Marfull <benja.marfull@gmail.com>
2025-11-05 19:22:12 -0300

    se agrega estructura backend (Java Springboot)

---
+++ backEnd/src/test/java/cl/duoc/Level_Up/LevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.Level_Up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
